15da2fd0958478fd377f109b8d44b172e33f29a2
yh1120 <yonghun1120@gmail.com>
2021-09-27 11:29:51 +0900

    [#21] feat: LogOut Implemented

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from 'Assets/Chatpong_logo_trans.png';
 import { style } from './NavbarStyle';
+import { getAuth, onAuthStateChanged, signOut } from '@firebase/auth';
 
 const Navbar: React.FC = () => {
+  const [signCheck, setSignCheck] = useState<boolean>(false);
+
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth);
+    setSignCheck(false);
+  };
+
+  const validateLogIn = async () => {
+    const auth = getAuth();
+    await onAuthStateChanged(auth, (data) => {
+      if (data) {
+        setSignCheck(true);
+      } else {
+        setSignCheck(false);
+      }
+    });
+  };
+
+  useEffect(() => {
+    validateLogIn();
+  }, []);
+
   return (
     <Header>
       <Nav>
@@ -15,12 +39,18 @@ const Navbar: React.FC = () => {
           <NavLink to="/">Game</NavLink>
         </NavMenu>
         <NavBtn>
-          <NavBtnLink to="/signin" background="transparent" color="#611f66">
-            로그인
-          </NavBtnLink>
-          <NavBtnLink to="/signup" background="#611f66" color="#fff">
-            회원가입
-          </NavBtnLink>
+          {signCheck ? (
+            <button onClick={handleSignOut}>로그아웃</button>
+          ) : (
+            <>
+              <NavBtnLink to="/signin" background="transparent" color="#611f66">
+                로그인
+              </NavBtnLink>
+              <NavBtnLink to="/signup" background="#611f66" color="#fff">
+                회원가입
+              </NavBtnLink>
+            </>
+          )}
         </NavBtn>
       </Nav>
     </Header>
