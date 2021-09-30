fd5aa2d458bf73859d2163799d325f504c56165c
hurima90-kim <hurima90@gmail.com>
2021-09-30 14:25:00 +0900

    feat: Firebase Storage 초기 세팅

--- src/fBase.js
+++ src/fBase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,4 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore();
+export const db = getFirestore(app);
+export const storage = getStorage();
+// 'gs://chatpong-42469.appspot.com'
