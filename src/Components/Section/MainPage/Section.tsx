c6fe6c7544e67d56d96036903a02cdbcc1422b53
kwak-bs <iam-bs@naver.com>
2021-11-20 20:43:18 +0900

    Refactor : Modify Section Ment

--- src/Components/Section/MainPage/Section.tsx
+++ src/Components/Section/MainPage/Section.tsx
@@ -8,11 +8,8 @@ const Section: React.FC = () => {
     <Container>
       <InnerContainer>
         <Content>
-          <Title>Slack은 미래의 일이 이뤄지는 곳 입니다.</Title>
-          <SubTitle>
-            일을 완료하는 데 필요한 모든 사람과 모든 항목을 위해 한 곳에서
-            작업하는 방식을 바꿔보세요
-          </SubTitle>
+          <Title>ChatPong은 협업의 미래입니다.</Title>
+          <SubTitle>메신저 플랫폼의 신세계를 경험해보세요.</SubTitle>
           <ChatBtn to="/chat/lobby">채팅하기</ChatBtn>
         </Content>
         <IntroImg>
